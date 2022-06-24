ca63b0ba8eff4cfbcdd603251c8725afe0718c4e
ashlchen <ashlchen@iu.edu>
2022-06-24 08:04:13 -0400

    practiced linked list

--- array.py
+++ array.py
@@ -46,3 +46,118 @@ class Solution:
         #     ret.append(heapq.heappop(heap)[1])
             
         # return ret
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        #input: a list of integer
+        # output: a list of integer (product)
+        
+        #min length is 2
+        # might have 0 or negative numbers
+        
+        #match
+        # n^2 solution
+        # for loop * for loop
+        
+        # n solution -- caculate left and right portion separately
+        n = len(nums)
+        res = [None] * n
+        left = 1
+        for i in range(n):
+            if i > 0:
+                left = left * nums[i-1]
+            res[i] = left
+        right = 1
+        for i in range(n-1, -1, -1):
+            if i < (n-1):
+                right = right * nums[i+1]
+            res[i] *= right
+        return res
+
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # if not nums: # if empty
+        #     return 0
+        # nums= list(set(nums)) # takes care duplicates
+        # heapq.heapify(nums) # heapify the list
+        # count = 1
+        # temp = 1
+        # first = heapq.heappop(nums) 
+        # while nums:
+        #     second = heapq.heappop(nums)
+        #     if second - first == 1:
+        #         temp +=1
+        #     else:
+        #         count = max(count, temp)
+        #         temp = 1
+        #     first = second
+        # count = max(count,temp)
+        # return count
+            
+        count = 1
+        temp = 1
+        if not nums:
+            return 0
+        nums = set(nums)
+        for n in nums:
+            if n-1 not in nums:
+                y = n+1
+                while y in nums:
+                    temp +=1
+                    y += 1
+                count = max(count, temp)
+                temp = 1
+        return count
+
+    def threeSum(nums):
+        # input: a list of number
+        # output; a list of lists
+        # there might be duplicates
+        # if input length < 3 return empty res
+
+        # match
+        # sort and sliding window
+        res = []
+        if not nums or len(nums)<3:
+            return res
+        nums = sorted(nums)
+        for i, num in enumerate(nums):
+            if num > 0:
+                return res
+            l, r = i+1, len(nums)-1
+            while l<r:
+                temp = num + nums[l] + nums[r]
+                if temp > 0:
+                    r -=1
+                elif temp == 0:
+                    if [num,nums[l],nums[r]] not in res:
+                        res.append([num,nums[l],nums[r]])
+                else:
+                    l += 1
+        return res
+
+        # edge
+        # [0,0,0,0]
+
+    def maxArea(height):
+        # understand
+        # area = width * height
+        # height = min(height[i], height[j])
+        # width = j - i
+        # maximize both height and width
+
+        # min of 2 elements in the list
+        # height[i] can be 0
+
+        # match
+        # 2 pointers
+
+        # plan
+        res = 0 
+        l, r = 0, len(height)-1
+        while l < r:
+            cur = (r-l) * min(height[r], height[l])
+            res = max(cur, res)
+            if height[l] <= height[r]:
+                l +=1
+            else:
+                r -= 1
+        return res
+        
--- linkedList.py
+++ linkedList.py
@@ -26,3 +26,34 @@ c = Node(3)
 a.next = b
 b.next = c
 printList(a)
+def reorderList(self, head: Optional[ListNode]) -> None:
+    """
+    Do not return anything, modify head in-place instead.
+    """
+    #multiple pass
+    
+    # find first section and second section
+    slow, fast = head, head.next
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    second = slow.next
+    prev = slow.next = None
+    # change second section direction
+    while second:
+        temp = second.next
+        second.next = prev
+        prev = second
+        second = temp
+    
+    cur = head
+    while prev:
+        temp1 = cur.next
+        temp2 = prev.next
+        cur.next = prev
+        prev.next = temp1
+        prev = temp2
+        cur = temp1
+        
+    
+    
