a34919c4d24a5272e463ba3b3ae6b316e444aa7b
ashlchen <ashlchen@iu.edu>
2022-06-30 21:01:17 -0400

    practiced reverse doubly list

--- Topics/linkedList.py
+++ Topics/linkedList.py
@@ -1,79 +1,75 @@
 
 class Node:
-    def __init__(self, val = 0, next=None):
+    def __init__(self, val = 0, prev = None, next=None):
         self.val = val
         self.next = next
+        self.prev = prev
 
-# def remove_element(head, target):
-#     dummy = Node()
-#     cur = dummy
-#     dummy.next = head
-#     while cur:
-#         if cur.next.val == target:
-#             cur.next = cur.next.next
-#         else:
-#             cur = cur.next
-#     return dummy.next
+def remove_element(head, target):
+    dummy = Node()
+    cur = dummy
+    dummy.next = head
+    while cur:
+        if cur.next.val == target:
+            cur.next = cur.next.next
+        else:
+            cur = cur.next
+    return dummy.next
 
-# def printList(node):
-#     while node:
-#         print(node.val, end=" ")
-#         node = node.next
+def printList(node):
+    while node:
+        print(node.val, end=" ")
+        node = node.next
 
 
-# a = Node(1)
-# b = Node(2)
-# c = Node(3)
-# a.next = b
-# b.next = c
-# printList(a)
-# def reorderList(self, head: Optional[ListNode]) -> None:
-#     """
-#     Do not return anything, modify head in-place instead.
-#     """
-#     #multiple pass
+
+def reorderList(self, head: Optional[ListNode]) -> None:
+    """
+    Do not return anything, modify head in-place instead.
+    """
+    #multiple pass
     
-#     # find first section and second section
-#     slow, fast = head, head.next
-#     while fast and fast.next:
-#         slow = slow.next
-#         fast = fast.next.next
-#     second = slow.next
-#     prev = slow.next = None
-#     # change second section direction
-#     while second:
-#         temp = second.next
-#         second.next = prev
-#         prev = second
-#         second = temp
+    # find first section and second section
+    slow, fast = head, head.next
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    second = slow.next
+    prev = slow.next = None
+    # change second section direction
+    while second:
+        temp = second.next
+        second.next = prev
+        prev = second
+        second = temp
     
-#     cur = head
-#     while prev:
-#         temp1 = cur.next
-#         temp2 = prev.next
-#         cur.next = prev
-#         prev.next = temp1
-#         prev = temp2
-#         cur = temp1
+    cur = head
+    while prev:
+        temp1 = cur.next
+        temp2 = prev.next
+        cur.next = prev
+        prev.next = temp1
+        prev = temp2
+        cur = temp1
         
     
-#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]: 
-#         dummy = ListNode()
-#         dummy.next = head
-#         # first pass to find the total length
-#         curLen = 0
-#         pointer = dummy
-#         while pointer.next:
-#             pointer = pointer.next
-#             curLen += 1
-#         # find target node counting from front
-#         remove_node = curLen - n
-#         cur = dummy
-#         # move cur to the node before remove_node
-#         for i in range(remove_node):
-#             cur = cur.next
-#         cur.next = cur.next.next
-#         return dummy.next
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]: 
+        dummy = ListNode()
+        dummy.next = head
+        # first pass to find the total length
+        curLen = 0
+        pointer = dummy
+        while pointer.next:
+            pointer = pointer.next
+            curLen += 1
+        # find target node counting from front
+        remove_node = curLen - n
+        cur = dummy
+        # move cur to the node before remove_node
+        for i in range(remove_node):
+            cur = cur.next
+        cur.next = cur.next.next
+        return dummy.next
 
 
 def isPalindrome(root):
@@ -109,7 +105,7 @@ def isPalindrome(root):
             return False
     return True
 
-def testCase():
+
     root = Node(1)
     a = Node(2)
     b = Node(2)
@@ -150,3 +146,6 @@ def reversePair(head):
         cur = new_section
     return head
 
+def reverseDoublyList(head):
+    
+def testCase():
