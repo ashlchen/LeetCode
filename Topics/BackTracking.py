b6ba208190952f095e049b7acec0291812a2308f
ashlchen <ashlchen@iu.edu>
2022-07-02 22:33:48 -0400

    Practiced backtracking

--- Topics/BackTracking.py
+++ Topics/BackTracking.py
@@ -1,6 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        for i, num in enumerate(nums):
-            temp = num
-            
+        res = [[]]
+        def backtrack(nums):
+            if not nums:
+                return
+            # add to the result
+            if nums not in res:
+                res.append(nums.copy())
+            # take out each of the element
+                for i, num in enumerate(nums):
+                # call backtrack
+                    backtrack(nums[:i] + nums[i+1:])
+        backtrack(nums)
+        return res
