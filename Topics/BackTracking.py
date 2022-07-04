762ac85d657cf10278f7fe4f70feb47c48a3f021
ashlchen <ashlchen@iu.edu>
2022-07-04 13:30:13 -0400

    Practiced backtracing

--- Topics/BackTracking.py
+++ Topics/BackTracking.py
@@ -1,5 +1,6 @@
+
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsets(self, nums):
         res = [[]]
         def backtrack(nums):
             if not nums:
@@ -13,3 +14,23 @@ class Solution:
                     backtrack(nums[:i] + nums[i+1:])
         backtrack(nums)
         return res
+
+    def combinationSum(self, nums, target):
+        result = []
+        def backtrack(nextNumIndex, curArray, curSum):
+            if curSum == target:
+                result.append(curArray.copy())
+                return
+            elif curSum > target or nextNumIndex >= len(nums):
+                return
+            curArray.append(nums[nextNumIndex])
+            backtrack(nextNumIndex, curArray, curSum+nums[nextNumIndex])
+            curArray.pop()
+            backtrack(nextNumIndex+1, curArray, curSum)
+        backtrack(0, [], 0)
+        return result
+    
+Object = Solution()
+print(Object.combinationSum([2,3,5], 7))
+
+
